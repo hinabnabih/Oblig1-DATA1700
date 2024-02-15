3e3a0c6fae69d424df7e5e1ba3c4ce4a0de3a7a8
Hiban <hinab4897@oslomet.no>
2024-02-15 23:47:41 +0100

    if-setning, og kommentarer

--- .idea/oblig.js
+++ .idea/oblig.js
@@ -1,22 +1,27 @@
+let bilettArray=[]; //Oppretter et array
+function leggTilBiletter(kino) { // for å legge til biletter
+    kino.preventDefault();
 
-let bilettArray=[];
-function validerSkjema(kino){
-    kino.preventDefault(
+    //henter verdiene
+    let film = document.getElementById('film').value;
+    let kvantitet = document.getElementById('film').value;
+    let fornavn = document.getElementById('film').value;
+    let etternavn = document.getElementById('film').value;
+    let telefonnummer = document.getElementById('film').value;
+    let email = document.getElementById('film').value;
 
-        let film=document.getElementById('film').value;
-        let kvantitet=document.getElementById('film').value;
-        let fornavn=document.getElementById('film').value;
-        let etternavn=document.getElementById('film').value;
-        let telefonnummer=document.getElementById('film').value;
-        let email=document.getElementById('film').value;
 
-        if (film && kvantitet && fornavn && etternavn && telefonnummer && email) {
-            let bilett = {
-                film: film,
-                kvantitet: kvantitet,
-                fornavn: fornavn,
-                etternavn: etternavn,
-                telefonnummer: telefonnummer,
-                email: email
-            };
-            bilettArray.push(bilett);
+    if (film && kvantitet && fornavn && etternavn && telefonnummer && email) { //if-setning som passer på at alle felt er fylt ut
+        //når flere biletter blir bestilt
+        let bilett = {
+            film: film,
+            kvantitet: kvantitet,
+            fornavn: fornavn,
+            etternavn: etternavn,
+            telefonnummer: telefonnummer,
+            email: email
+        };
+        //legger til de nye bilettene i arrayet
+        bilettArray.push(bilett);
+        fordelBiletter();
+
